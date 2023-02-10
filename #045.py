922b8ed4314634583f212dd5d2d44803b4d4e1f1
fesprada <fesprada@gmail.com>
2023-02-10 22:41:34 +0000

    actualizaçao ate ex45

--- #045.py
+++ #045.py
@@ -9,7 +9,7 @@ print(''' suas opções:
 [0] Pedra
 [1] Papel
 [2] Tesoura ''')
-jogador = int(input('qual a sua opção?'))
+jogador = int(input('qual a sua opção? '))
 print('computador jogou {}'.format(itens[computador]))
 print('jogador jogou {}.'.format(itens[jogador]))
 if computador == 0:
@@ -19,6 +19,8 @@ if computador == 0:
         print('ganhou')
     elif jogador == 2:
         print('perdeu')
+    else:
+        print('jogada inválida')
 elif computador == 1:
     if jogador == 0:
         print('perdeu')
@@ -26,6 +28,8 @@ elif computador == 1:
         print('empate')
     elif jogador == 2:
         print('ganhou')
+    else:
+        print('jogada inválida')
 elif computador == 2:
     if jogador == 0:
         print('ganhou')
@@ -33,3 +37,5 @@ elif computador == 2:
         print('perdeu')
     elif jogador == 2:
         print('empate')
+    else:
+        print('jogada inválida')
